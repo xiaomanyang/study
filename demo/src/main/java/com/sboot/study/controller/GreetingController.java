ca35fc321bbe8f3eee4fe8f2bb8c178bca6a8125
wangchunlai <wang.c.l@bimtechnologie.com>
2019-10-09 15:10:24 +0800

    mybatis分页插件 druid数据源 mybatis日志

--- demo/src/main/java/com/sboot/study/controller/GreetingController.java
+++ demo/src/main/java/com/sboot/study/controller/GreetingController.java
@@ -1,14 +1,18 @@
 package com.sboot.study.controller;
 
 import com.alibaba.fastjson.JSONObject;
+import com.github.pagehelper.PageHelper;
+import com.github.pagehelper.PageInfo;
 import com.sboot.study.dao.UserMapper;
 import com.sboot.study.domain.Greeting;
+import com.sboot.study.domain.User;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.List;
 import java.util.concurrent.atomic.AtomicLong;
 
 /**
@@ -38,7 +42,9 @@ public class GreetingController {
     public JSONObject users(){
         JSONObject j = new JSONObject();
         j.put("user", userMapper.selectByPrimaryKey(1));
-        j.put("usersm", userMapper.selectAll());
+        PageHelper.startPage(1,20);
+        List<User> users = userMapper.selectAll();
+        j.put("usersm", new PageInfo(users));
         /*String sql="select * from sys_user";
         RowMapper<SysUser> rowMapper=new BeanPropertyRowMapper<SysUser>(SysUser.class);
         List users= jdbcTemplate.query(sql, rowMapper);
