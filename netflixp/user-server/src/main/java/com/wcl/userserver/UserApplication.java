e73b4a0d2aef38744b0224fb53c11ec0cec4037c
wangchunlai <wang.c.l@bimtechnologie.com>
2021-04-22 15:51:16 +0800

    mybatis-plus

--- netflixp/user-server/src/main/java/com/wcl/userserver/UserApplication.java
+++ netflixp/user-server/src/main/java/com/wcl/userserver/UserApplication.java
@@ -1,11 +1,13 @@
 package com.wcl.userserver;
 
+import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
 @SpringBootApplication
 @EnableEurekaClient
+@MapperScan("com.wcl.userserver.sys.mapper")
 public class UserApplication {
 
     public static void main(String[] args) {
