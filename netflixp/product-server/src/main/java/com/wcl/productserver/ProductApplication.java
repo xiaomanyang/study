1d7169497128e7fa54522fc52c8f00cf139e78a6
wangchunlai <wang.c.l@bimtechnologie.com>
2021-04-23 18:06:20 +0800

    mybatis-plus

--- netflixp/product-server/src/main/java/com/wcl/productserver/ProductApplication.java
+++ netflixp/product-server/src/main/java/com/wcl/productserver/ProductApplication.java
@@ -1,10 +1,13 @@
 package com.wcl.productserver;
 
+import com.netflix.loadbalancer.IRule;
+import com.netflix.loadbalancer.RoundRobinRule;
 import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 import org.springframework.cloud.openfeign.EnableFeignClients;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 @EnableEurekaClient
@@ -14,6 +17,11 @@ public class ProductApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(ProductApplication.class, args);
+    }
+
+    @Bean
+    public IRule myRule(){
+        return new RoundRobinRule();
     }
 
 }
